26ea22364866d7ad4506f8aed142b2a3de840b07
Andriy Palyukha <apalyukha@gmail.com>
2020-05-21 09:08:29 +0300

    Making multiple HTTP requests using Python

---
+++ src/example_async_http_requests/sample_test_multithreading.py
@@ -0,0 +1,19 @@
+import requests
+from timer import timer
+from concurrent.futures import ThreadPoolExecutor
+
+
+URL = 'https://httpbin.org/uuid'
+
+
+def fetch(session, url):
+    with session.get(url) as response:
+        print(response.json()['uuid'])
+
+
+@timer(1, 5)
+def main():
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        with requests.Session() as session:
+            executor.map(fetch, [session] * 100, [URL] * 100)
+            executor.shutdown(wait=True)
